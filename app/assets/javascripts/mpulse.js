5ba47d22277ab57dff506ed9a04fc0d7db20c328
polarpine <vanessabiggers@gmail.com>
2014-11-14 20:11:16 -0800

    user login to spotify added

--- app/assets/javascripts/mpulse.js
+++ app/assets/javascripts/mpulse.js
@@ -1,19 +1,39 @@
+var scopes = 'user-read-private playlist-read-private user-library-modify user-library-read playlist-modify-private playlist-modify-public'
+var my_client_id = '580cbd7871db4617af3efba743122a64'; // Your client id
+var my_secret = '41e55c17fba34ef68da571cb70207481'; // Your secret
+var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+
 
 var findSong = function (query) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/tracks/' + query,
-        success: function (response) {
-        $("#songHolder").html("You found: " + response.artists[0].name)
-        console.log(response)
-        }
-    });
+  $.ajax({
+    url: 'https://api.spotify.com/v1/tracks/' + query,
+    success: function (response) {
+      $("#songHolder").html("You found: " + response.artists[0].name)
+      console.log(response)
+    }
+  });
 };
 
 angular.module('song', [])
-  .controller('SongController', function() {
-    this.id = '3L7BcXHCG8uT92viO6Tikl';
-    this.find = function getSong() {
-      console.log(this);
-      findSong(this.id)
-    };
-  });
+.controller('SongController', function() {
+  this.id = '3L7BcXHCG8uT92viO6Tikl';
+  this.login = function logUserIn() {
+    userLogin()
+    console.log('you are logging in to Spotify');
+  }; 
+  this.find = function getSong() {
+    console.log(this);
+    findSong(this.id)
+  };
+});
+
+
+
+
+
+var userLogin = function() {
+  var url = 'https://accounts.spotify.com/authorize/?client_id=' + my_client_id + '&response_type=code&redirect_uri=' + redirect_uri + '&scope=' + scopes
+  var w = window.open(url, '_parent', 'scrollbars=yes, resizable=yes, top=500, left=500, width=400, height=400');
+}
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'mpulse#index'
 
+  get 'callback' => 'mpulse#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
