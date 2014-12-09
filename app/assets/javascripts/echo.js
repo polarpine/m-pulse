a2ae957ffa7b0d0c2be133d9684deec7c8b33dba
polarpine <vanessabiggers@gmail.com>
2014-12-09 12:55:59 -0800

    add find song by tempo to view

--- app/assets/javascripts/echo.js
+++ app/assets/javascripts/echo.js
@@ -5,16 +5,23 @@
 "http://developer.echonest.com/api/v4/song/search?api_key=XVGKI8UKHAE9MSPJ5&style=rock&max_tempo=150&min_tempo=140&results=10"
 
 var findTempo = function (min, max) {
-  $.ajax({
-    url: 'http://developer.echonest.com/api/v4/song/search?api_key=XVGKI8UKHAE9MSPJ5&max_tempo='+ max + '&min_tempo='+ min +'&results=6&bucket=id:spotify&bucket=tracks&limit=true',
-    success: function (response) {
-    	var song_id = response.response.songs[0].tracks[0].foreign_id.slice(14)
-    	console.log(song_id)
-    	findSong(song_id)
-
-
-
-
-    }
-  });
+	$.ajax({
+		url: 'http://developer.echonest.com/api/v4/song/search?api_key=XVGKI8UKHAE9MSPJ5&max_tempo='+ max + '&min_tempo='+ min +'&results=6&bucket=id:spotify&bucket=tracks&limit=true',
+		success: function (response) {
+			var song_id = response.response.songs[0].tracks[0].foreign_id.slice(14)
+			console.log(song_id)
+			findSong(song_id)
+		}
+	});
 };
+
+
+angular.module('tempo', [])
+.controller('TempoController', function() {
+	this.min = 300;
+	this.max = 500;
+	this.find = function getTemp() {
+		console.log(this);
+		findSong(this.min, this.max)
+	};
+});
