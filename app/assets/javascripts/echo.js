94628d4f58908e0ae925dcefea2fcc9417847b7c
polarpine <vanessabiggers@gmail.com>
2015-03-01 15:55:37 -0800

    add getTempo of song from echonest API call

--- app/assets/javascripts/echo.js
+++ app/assets/javascripts/echo.js
@@ -1,25 +1,3 @@
-//Specs for Spotify API
-var scopes = 'user-read-private playlist-read-private user-library-modify user-library-read playlist-modify-private playlist-modify-public'
-var my_client_id = '580cbd7871db4617af3efba743122a64'; // Your client id
-var my_secret = '41e55c17fba34ef68da571cb70207481'; // Your secret
-var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
-
-//Simple Find Song Method
-var findSong = function (query) {
-  query = query || "34gCuhDGsG4bRPIf9bb02f"; //Song for Melissa :)
-  $.ajax({
-    url: 'https://api.spotify.com/v1/tracks/' + query,
-    success: function (response) {
-      $("#songHolder").html("You found: " + response.artists[0].name + " " + response.name)
-      $("iframe").attr('src', $('iframe').attr('src') + response.id)
-      $("#songPlayer").show()
-      console.log(response)
-    }
-  });
-};
-
-
-
 
 //Login to Spotify
 
@@ -44,8 +22,8 @@ var findSong = function (query) {
       // $("#songHolder").html("You found: " + response.artists[0].name + " " + response.name)
       // $("iframe").attr('src', $('iframe').attr('src') + response.id)
       // $("#songPlayer").show()
-      console.log("Return from Spotify:")
-      console.log(response.name + " by " + response.artists[0].name )
+
+      console.log("From Spotify: " + response.name + " by " + response.artists[0].name )
     }
   });
 };
@@ -59,19 +37,32 @@ var findSong = function (query) {
 
 "http://developer.echonest.com/api/v4/song/search?api_key=XVGKI8UKHAE9MSPJ5&style=rock&max_tempo=150&min_tempo=140&results=10"
 
-var findTempo = function (min, max, style) {
-	min = min || '50'
-	max = max || '300'
+var findSongsByTempo = function (min, max, style) {
+	min = min || '200'
+	max = max || '330'
 	style = style || 'rock'
 	$.ajax({
-		url: 'http://developer.echonest.com/api/v4/song/search?api_key=XVGKI8UKHAE9MSPJ5&style='+ style +'&max_tempo=' + max + '&min_tempo='+ min +'&results=6&bucket=id:spotify&bucket=tracks&limit=true',
+		url: 'http://developer.echonest.com/api/v4/song/search?api_key=XVGKI8UKHAE9MSPJ5&style='+ style +'&max_tempo=' + max + '&min_tempo='+ min +'&results=10&bucket=id:spotify&bucket=tracks&limit=true',
 		success: function (response) {
 			console.log(response)
 			songs = response.response.songs
-			var song_id = response.response.songs[0].tracks[0].foreign_id.slice(14)
 			for (i = 0; i < songs.length ; i++) {
     			findSong(songs[i].tracks[0].foreign_id.slice(14));
 				}
+		
+		}
+	});
+};
+
+
+var findTempoOfSong = function (spotify_id) {
+	spotify_id = spotify_id || '34gCuhDGsG4bRPIf9bb02f'
+	$.ajax({
+		url:'http://developer.echonest.com/api/v4/song/profile?api_key=XVGKI8UKHAE9MSPJ5&track_id=spotify:track:'+ spotify_id +'&bucket=id:spotify&limit=true&bucket=audio_summary',
+		success: function (response) {
+			song = response.response.songs[0]
+			console.log(song.title + " by " + song.artist_name + " has " + song.audio_summary.tempo + " bpms")
+
 		
 		}
 	});
