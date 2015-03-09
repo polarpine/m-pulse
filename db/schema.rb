13a6a2d20c320ddd64c3c98b7b588617bd82dfb6
Lawrence Ma <lawrencema@lawrences-air.att.net>
2015-03-08 23:10:26 -0700

    adds mockup of playlists

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 20150308184241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "mpulses", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "songs", force: true do |t|
     t.string   "name"
     t.string   "artist"
